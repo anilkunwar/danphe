e3acb951905cae431059e2c98db94246c0c1fa57
Anil Kunwar <anilkunwar318@yahoo.com>
2015-06-21 03:21:31 +0800

    Update MultiRectangleBoxIC.C

--- src/ics/MultiRectangleBoxIC.C
+++ src/ics/MultiRectangleBoxIC.C
@@ -4,7 +4,7 @@
 /*          All contents are licensed under LGPL V2.1           */
 /*             See LICENSE for full restrictions                */
 /****************************************************************/
-#include "RndBoundingBoxIC.h"
+#include "MultiRectangleBoxIC.h"
 #include "MooseRandom.h"
 
 template<>
