293b2ff5325d0da242324664bed2bc8a709a44c8
Anil Kunwar <anilkunwar318@yahoo.com>
2015-07-01 22:32:58 +0800

    Update SplitCHVoltage.C

--- src/kernel/SplitCHVoltage.C
+++ src/kernel/SplitCHVoltage.C
@@ -4,19 +4,19 @@
 /* All contents are licensed under LGPL V2.1 */
 /* See LICENSE for full restrictions */
 /****************************************************************/
-#include "SplitCHSoret.h"
+#include "SplitCHVoltage.h"
 template<>
-InputParameters validParams<SplitCHSoret>()
+InputParameters validParams<SplitCHVoltage>()
 {
 InputParameters params = validParams<Kernel>();
 params.addClassDescription("Add Soret effect to Split formulation Cahn-Hilliard Kernel");
-params.addRequiredCoupledVar("T", "Temperature");
+params.addRequiredCoupledVar("T", "Voltage");
 params.addRequiredCoupledVar("c", "Concentration");
 params.addRequiredParam<MaterialPropertyName>("diff_name", "The diffusivity used with the kernel");
 params.addParam<MaterialPropertyName>("Q_name", "Qheat", "The material name for the heat of transport");
 return params;
 }
-SplitCHSoret::SplitCHSoret(const std::string & name, InputParameters parameters) :
+SplitCHVoltage::SplitCHVoltage(const std::string & name, InputParameters parameters) :
 Kernel(name, parameters),
 _T_var(coupled("T")),
 _T(coupledValue("T")),
@@ -29,13 +29,13 @@ _kb(8.617343e-5) // Boltzmann constant in eV/K
 {
 }
 Real
-SplitCHSoret::computeQpResidual()
+SplitCHVoltage::computeQpResidual()
 {
 Real T_term = _D[_qp] * _Q[_qp] * _c[_qp] / (_kb * _T[_qp] * _T[_qp]);
 return T_term * _grad_T[_qp] * _grad_test[_i][_qp];
 }
 Real
-SplitCHSoret::computeQpOffDiagJacobian(unsigned int jvar)
+SplitCHVoltage::computeQpOffDiagJacobian(unsigned int jvar)
 {
 if (_c_var == jvar)
 return _D[_qp] * _Q[_qp] * _phi[_j][_qp] * _grad_T[_qp] / (_kb * _T[_qp] * _T[_qp]) * _grad_test[_i][_qp];
