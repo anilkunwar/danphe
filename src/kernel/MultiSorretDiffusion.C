97df03b4ddf7788ad84b0e36eeae21c84e715d16
Anil Kunwar <anilkunwar318@yahoo.com>
2015-10-16 11:22:35 +0800

    Update MultiSorretDiffusion.C

--- src/kernel/MultiSorretDiffusion.C
+++ src/kernel/MultiSorretDiffusion.C
@@ -12,8 +12,8 @@ InputParameters validParams<SoretDiffusion>()
   params.addClassDescription("Add Soret effect to Split formulation Cahn-Hilliard Kernel");
   params.addRequiredCoupledVar("T", "Temperature");
   params.addRequiredCoupledVar("c", "Concentration");
-  params.addRequiredParam<MaterialPropertyName>("diff_name", "The diffusivity used with the kernel");
-  #params.addRequiredParam<MaterialPropertyName>("diff_name", "The diffusivity used with the kernel");
+  params.addRequiredParam<MaterialPropertyName>("diff_name_1", "The diffusivity of Cu used with the kernel");
+  params.addRequiredParam<MaterialPropertyName>("diff_name_2", "The diffusivity of Sn used with the kernel");
   params.addParam<MaterialPropertyName>("Q_name", "Qheat", "The material name for the heat of transport");
   #params.addParam<MaterialPropertyName>("Q_name", "Qheat", "The material name for the heat of transport");
   return params;
