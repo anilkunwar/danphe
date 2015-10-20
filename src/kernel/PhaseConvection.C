acc7947a67609d2efe68b90f7e621af60f9845eb
Anil Kunwar <anilkunwar318@yahoo.com>
2015-10-20 21:53:25 +0800

    Update PhaseConvection.C

--- src/kernel/PhaseConvection.C
+++ src/kernel/PhaseConvection.C
@@ -12,33 +12,33 @@
 /*            See COPYRIGHT for full restrictions               */
 /****************************************************************/
 
-#include "ExampleConvection.h"
+#include "PhaseConvection.h"
 
 /**
  * This function defines the valid parameters for
  * this Kernel and their default values
  */
 template<>
-InputParameters validParams<ExampleConvection>()
+InputParameters validParams<PhaseConvection>()
 {
   InputParameters params = validParams<Kernel>();
   params.addRequiredParam<RealVectorValue>("velocity", "Velocity Vector");
   return params;
 }
 
-ExampleConvection::ExampleConvection(const InputParameters & parameters) :
+PhaseConvection::PhaseConvection(const InputParameters & parameters) :
   // You must call the constructor of the base class first
   Kernel(parameters),
    _velocity(getParam<RealVectorValue>("velocity"))
 {}
 
-Real ExampleConvection::computeQpResidual()
+Real PhaseConvection::computeQpResidual()
 {
   // velocity * _grad_u[_qp] is actually doing a dot product
   return _test[_i][_qp]*(_velocity*_grad_u[_qp]);
 }
 
-Real ExampleConvection::computeQpJacobian()
+Real PhaseConvection::computeQpJacobian()
 {
   // the partial derivative of _grad_u is just _grad_phi[_j]
   return _test[_i][_qp]*(_velocity*_grad_phi[_j][_qp]);
