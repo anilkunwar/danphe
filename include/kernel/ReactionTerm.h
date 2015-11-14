146dedb0dcf5c016187f4d0101215e873998a7a6
Anil Kunwar <anilkunwar318@yahoo.com>
2015-11-14 22:45:48 +0800

    Update ReactionTerm.h

--- include/kernel/ReactionTerm.h
+++ include/kernel/ReactionTerm.h
@@ -1,3 +1,4 @@
+
 /****************************************************************/
 /*               DO NOT MODIFY THIS HEADER                      */
 /* MOOSE - Multiphysics Object Oriented Simulation Environment  */
@@ -12,63 +13,49 @@
 /*            See COPYRIGHT for full restrictions               */
 /****************************************************************/
 
-#include "ReactionTerm.h"
+#ifndef REACTIONTERM_H
+#define REACTIONTERM_H
+
+#include "Kernel.h"
+
+// Forward Declaration
+class ReactionTerm;
 
 template<>
-InputParameters validParams<ReactionTerm>()
+InputParameters validParams<ReactionTerm>();
+
+/**
+ * Kernel which implements the convective term in the transient heat
+ * conduction equation, and provides coupling with the Darcy pressure
+ * equation.
+ */
+class ReactionTerm : public Kernel
 {
-  InputParameters params = validParams<Kernel>();
+public:
+  ReactionTerm(const InputParameters & parameters);
 
-  params.addRequiredCoupledVar("Temperature", "The variable representing the temperature.");
-  // Add a required parameter.  If this isn't provided in the input file MOOSE will error.
-  params.addRequiredParam<MaterialPropertyName>("c_sat", "The saturated solubility used with the kernel");
-  params.addRequiredParam<Real>("k_chem", "the product of chemical reaction constant and ration s/v");
+protected:
+  virtual Real computeQpResidual();
 
-  // Add a parameter with a default value.  This value can be overriden in the input file.
-  //params.addParam<Real>("kb", 1.38e-23, "The Boltzmann constant in J/K");
+  virtual Real computeQpJacobian();
 
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
 
-  return params;
-}
+  /// int label for temperature variable
+  unsigned int _T_var;
 
-ReactionTerm::ReactionTerm(const InputParameters & parameters) :
-    Kernel(parameters),
-    // Save off the coupled variable identifier for use in
-    // computeQpOffDiagJacobian
-    _T_var(coupled("Temperature")),
-    // Save off the coupled value for use in Residual 
-    _T(coupledValue("Temperature")),
-    // Couple to the gradient of the pressure
-    _grad_T(coupledGradient("Temperature")),
-    // Grab necessary material properties
-    _cs(getMaterialProperty<Real>("c_sat")),
-    _kc(getParam<Real>("k_chem"))
-    //_kb(getParam<Real>("kb"))
-{
-}
+  /// Coupled variable for the temperature
+  VariableValue & _T;
 
-Real
-ReactionTerm::computeQpResidual()
-{
-  //return _kc *(_cs[_qp]- _u[_qp]) * _test[_i][_qp];
-  return - _kc * _u[_qp] * _test[_i][_qp];
-}
+  /// Variable gradient for temperature
+  VariableGradient & _grad_T;
 
-Real
-ReactionTerm::computeQpJacobian()
-{
- return  -_kc *  _phi[_j][_qp] * _test[_i][_qp]; 
-}
+  /// Diffusivity material property
+  const MaterialProperty<Real> & _cs;
 
-Real
-ReactionTerm::computeQpOffDiagJacobian(unsigned int jvar)
-{
-  if (jvar == _T_var)
-  {
-      0.0;
-  //  _D[_qp]*_Qh* (-_kb/_T[_qp]*_T[_qp])*(2*_grad_T[_qp]*_phi[_j][_qp]/ _T[_qp] +
-  //         _grad_phi[_j][_qp])*_grad_u[_qp] * _test[_i][_qp];
-  }
+  /// Will be set from the input file
+  //Real _Qh;
+  Real _kc;
+};
 
-  return 0.0;
-}
+#endif //REACTIONTERM_H
