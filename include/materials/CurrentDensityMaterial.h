c86985fe2348a21cdb55badfca286c984177b9d3
Anil Kunwar <anilkunwar318@yahoo.com>
2015-11-22 02:53:22 +0800

    Create CurrentDensityMaterial.h

---
+++ include/materials/CurrentDensityMaterial.h
@@ -0,0 +1,40 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+
+
+#ifndef CURRENTDENSITYMATERIAL_H
+#define CURRENTDENSITYMATERIAL_H
+
+#include "Material.h"
+
+//Forward Declarations
+class CurrentDensityMaterial;
+
+template<>
+InputParameters validParams<CurrentDensityMaterial>();
+
+/**
+ * Defines the permeability tensor used in Darcy flow
+ */
+class CurrentDensityMaterial : public Material
+{
+public:
+  CurrentDensityMaterial(const InputParameters & parameters);
+
+protected:
+
+  virtual void computeQpProperties();
+
+  /// current density tensor as entered by the user
+  RealTensorValue _j_mater_vol;
+
+  /// the Material property that this Material provides
+  MaterialProperty<RealTensorValue> & __current_density;
+
+};
+
+#endif //CURRENTDENSITYMATERIAL_H
