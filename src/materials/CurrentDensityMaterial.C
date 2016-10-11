d9fe54d34de87046aa5032cd1694848a982096e4
Anil Kunwar <anilkunwar318@yahoo.com>
2016-10-11 21:37:39 +0800

    Update CurrentDensityMaterial.C

--- src/materials/CurrentDensityMaterial.C
+++ src/materials/CurrentDensityMaterial.C
@@ -14,15 +14,16 @@ template<>
 InputParameters validParams<CurrentDensityMaterial>()
 {
   InputParameters params = validParams<Material>();
-  params.addRequiredParam<RealTensorValue>("j_tensor", "The material current_density tensor (in A/m^2)");
-  params.addClassDescription("Material that holds the current density tensor");
+  params.addRequiredParam<RealVectorValue>("j_vector", "The material current_density tensor (in A/m^2). E.g. '-1000 0 0'");
+  params.addClassDescription("Material that holds the current density vector and computes the drift velocity");
   return params;
 }
 
 CurrentDensityMaterial::CurrentDensityMaterial(const InputParameters & parameters) :
     Material(parameters),
-    _j_mater_vol(getParam<RealTensorValue>("j_tensor")),
-    _current_density(declareProperty<RealTensorValue>("current_density"))
+    _j_mater_vol(getParam<RealVectorValue>("j_vector")),
+    _current_density(declareProperty<RealVectorValue>("current_density"))
+       
 {
 }
 
