7ae6dd23f0e3d0abbb8001367d509ed666a11a02
Anil Kunwar <anilkunwar318@yahoo.com>
2018-02-15 14:28:58 +0800

    Update CHConvection.C

--- src/kernel/CHConvection.C
+++ src/kernel/CHConvection.C
@@ -24,12 +24,18 @@ validParams<CHConvection>()
 {
   InputParameters params = validParams<Kernel>();
  // params.addRequiredParam<RealVectorValue>("velocity", "Velocity Vector");
-   params.addCoupledVar(
-      "velocity_x", 0, "The variable containing the x-component of the velocity front.");
+  // params.addCoupledVar(
+   //   "velocity_x", 0, "The variable containing the x-component of the velocity front.");
+ // params.addCoupledVar(
+  //    "velocity_y", 0, "The variable containing the y-component of the velocity front.");
+ // params.addCoupledVar(
+	// "velocity_z", 0, "The variable containing the z-component of the velocity front.");
+	 params.addCoupledVar(
+      "u", 0, "The variable containing the x-component of the velocity front.");
   params.addCoupledVar(
-      "velocity_y", 0, "The variable containing the y-component of the velocity front.");
+      "v", 0, "The variable containing the y-component of the velocity front.");
   params.addCoupledVar(
-	"velocity_z", 0, "The variable containing the z-component of the velocity front.");
+	"w", 0, "The variable containing the z-component of the velocity front.");
   return params;
 }
 
@@ -37,12 +43,18 @@ CHConvection::CHConvection(const InputParameters & parameters)
   : // You must call the constructor of the base class first
     Kernel(parameters),
  //   _velocity(getParam<RealVectorValue>("velocity")),
-    _velocity_x(coupledValue("velocity_x")),
-    _velocity_y(coupledValue("velocity_y")),
-	_velocity_z(coupledValue("velocity_z")),
-	_x_vel_var(coupled("velocity_x")),
-    _y_vel_var(coupled("velocity_y")),
-	_z_vel_var(coupled("velocity_z"))
+  //  _velocity_x(coupledValue("velocity_x")),
+   // _velocity_y(coupledValue("velocity_y")),
+//	_velocity_z(coupledValue("velocity_z")),
+	_u_vel(coupledValue("u")),
+    	_v_vel(coupledValue("v")),
+	_w_vel(coupledValue("w")),
+//	_x_vel_var(coupled("velocity_x")),
+//    	_y_vel_var(coupled("velocity_y")),
+//	_z_vel_var(coupled("velocity_z"))
+	_u_vel_var(coupled("velocity_x")),
+    	_v_vel_var(coupled("velocity_y")),
+	_w_vel_var(coupled("velocity_z"))
 {
 //  _velocity(0) = _velocity_x[_qp];
  // _velocity(1) = _velocity_y[_qp];
@@ -53,9 +65,12 @@ CHConvection::CHConvection(const InputParameters & parameters)
 Real
 CHConvection::computeQpResidual()
 {
-  _velocity(0) = _velocity_x[_qp];
-  _velocity(1) = _velocity_y[_qp];
-  _velocity(2) = _velocity_z[_qp];
+ // _velocity(0) = _velocity_x[_qp];
+ //  _velocity(1) = _velocity_y[_qp];
+ //  _velocity(2) = _velocity_z[_qp];
+  _velocity(0) = _u_vel[_qp];
+  _velocity(1) = _v_vel[_qp];
+  _velocity(2) = _w_vel[_qp];
   
 //	const RealVectorValue vec(_velocity_x[_qp], _velocity_y[_qp], _velocity_z[_qp]);	
   // velocity * _grad_u[_qp] is actually doing a dot product
@@ -65,9 +80,12 @@ CHConvection::computeQpResidual()
 Real
 CHConvection::computeQpJacobian()
 {
-	_velocity(0) = _velocity_x[_qp];
-  _velocity(1) = _velocity_y[_qp];
-  _velocity(2) = _velocity_z[_qp];
+// _velocity(0) = _velocity_x[_qp];
+// _velocity(1) = _velocity_y[_qp];
+// _velocity(2) = _velocity_z[_qp];
+  _velocity(0) = _u_vel[_qp];
+  _velocity(1) = _v_vel[_qp];
+  _velocity(2) = _w_vel[_qp];
 //	const RealVectorValue vec(_velocity_x[_qp], _velocity_y[_qp], _velocity_z[_qp]);
   // the partial derivative of _grad_u is just _grad_phi[_j]
   return _test[_i][_qp] * (_velocity * _grad_phi[_j][_qp]);
