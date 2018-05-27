710195b1fdff79b268aa8bb94b4ce2e3361b7880
Anil Kunwar <anilkunwar318@yahoo.com>
2018-05-28 03:45:35 +0800

    Create SpreadingNeumannBC.C

---
+++ src/bcs/SpreadingNeumannBC.C
@@ -0,0 +1,37 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "CoupledNeumannBC.h"
+
+registerMooseObject("ExampleApp", SpreadingNeumannBC);
+
+template <>
+InputParameters
+validParams<SpreadingNeumannBC>()
+{
+  InputParameters params = validParams<IntegratedBC>();
+
+  // Here we are adding a parameter that will be extracted from the input file by the Parser
+  params.addParam<Real>("phi", 1.0, "Amplitude of the NeumannBC");
+  //params.addRequiredCoupledVar("some_var", "Flux Value at the Boundary");
+  return params;
+}
+
+SpreadingNeumannBC::SpreadingNeumannBC(const InputParameters & parameters)
+  : IntegratedBC(parameters),
+    _alpha(getParam<Real>("alpha")),
+    _some_var_val(coupledValue("some_var"))
+{
+}
+
+Real
+SpreadingNeumannBC::computeQpResidual()
+{
+  return _test[_i][_qp] * _phi * (_u[_qp]*_u[_qp]-1.0);
+}
